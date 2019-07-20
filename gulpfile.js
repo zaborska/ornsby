4d6eb2026bd5ae0995e071b483bfb15a1257a8ba
Yulia Zaborska <zaborska.yulia@gmail.com>
2019-07-20 20:50:38 +0300

    testimonals

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const watch = require("gulp-watch");
+
+// sass.compiler = require("node-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./css/"));
+});
+
+gulp.task("sass:watch", function() {
+  gulp.watch("./scss/**/*.scss", gulp.series("sass"));
+});
+
+gulp.task("default", gulp.series("sass", "sass:watch"));
